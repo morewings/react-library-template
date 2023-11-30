eec99807aed05554d86ee89b86ef844f6d277587
Dima Vyshniakov <dmytro.vyshniakov@olx.com>
2023-11-30 15:58:29 +0100

    init

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    // process `*.tsx` files with `ts-jest`
+  },
+  moduleNameMapper: {
+    '\\.(gif|ttf|eot|svg|png)$': 'identity-obj-proxy',
+    '\\.css$': 'identity-obj-proxy',
+  },
+};

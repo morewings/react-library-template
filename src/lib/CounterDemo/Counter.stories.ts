8dd835c8e20e8f1752c52ba78d1131bc300c0609
Dima Vyshniakov <morewings@gmail.com>
2023-12-03 00:00:46 +0100

    add postcss-preset-env

--- src/lib/CounterDemo/Counter.stories.ts
+++ src/lib/CounterDemo/Counter.stories.ts
@@ -3,25 +3,25 @@ import type {Meta, StoryObj} from '@storybook/react';
 import {Counter} from './Counter';
 
 const meta = {
-  title: 'Example/Counter',
-  component: Counter,
-  parameters: {
-    // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
-    layout: 'fullscreen',
-  },
+    title: 'Example/Counter',
+    component: Counter,
+    parameters: {
+        // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
+        layout: 'fullscreen',
+    },
 } as Meta<typeof Counter>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const ExampleCounter: Story = {
-  args: {
-    initialValue: 0,
-  },
+    args: {
+        initialValue: 0,
+    },
 };
 
 export const ExampleCounterWithInitialValue: Story = {
-  args: {
-    initialValue: 11,
-  },
+    args: {
+        initialValue: 11,
+    },
 };
--- vite.config.ts
+++ vite.config.ts
@@ -5,41 +5,39 @@ import react from '@vitejs/plugin-react';
 import hq from 'alias-hq';
 import external from '@yelo/rollup-node-external';
 import dts from 'vite-plugin-dts';
-import autoprefixer from 'autoprefixer';
+import postcssPresetEnv from 'postcss-preset-env';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: hq.get('rollup'),
-  },
-  plugins: [react(), dts({rollupTypes: true})],
-  build: {
-    sourcemap: true,
-    lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'src/lib/index.ts'),
-      name: 'Library name',
-      // the proper extensions will be added
-      fileName: 'index',
+    resolve: {
+        alias: hq.get('rollup'),
     },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: external(),
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          react: 'React',
+    plugins: [react(), dts({rollupTypes: true})],
+    build: {
+        sourcemap: true,
+        lib: {
+            // Could also be a dictionary or array of multiple entry points
+            entry: resolve(__dirname, 'src/lib/index.ts'),
+            name: 'Library name',
+            // the proper extensions will be added
+            fileName: 'index',
         },
-      },
+        rollupOptions: {
+            // make sure to externalize deps that shouldn't be bundled
+            // into your library
+            external: external(),
+            output: {
+                // Provide global variables to use in the UMD build
+                // for externalized deps
+                globals: {
+                    react: 'React',
+                },
+            },
+        },
     },
-  },
-  css: {
-    postcss: {
-      plugins: [
-        autoprefixer({}), // add options if needed
-      ],
+    css: {
+        postcss: {
+            plugins: [postcssPresetEnv({})],
+        },
     },
-  },
 });

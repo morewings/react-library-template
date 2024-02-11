54b5bdbd15b8de4aba86b82f52c517f79d56b9eb
Dima Vyshniakov <morewings@gmail.com>
2024-02-11 23:13:07 +0100

    Improve lint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
     root: true,
-    env: {browser: true, es2020: true},
+    env: {browser: true, es2020: true, node: true},
     parserOptions: {
         ecmaVersion: 'latest',
         // eslint-disable-next-line no-undef

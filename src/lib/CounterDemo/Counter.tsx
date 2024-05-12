0c6b993bb18370d659a2275baa7824422f88d2f6
Dima Vyshniakov <morewings@gmail.com>
2024-05-12 14:47:12 +0200

    Add Tailwind

--- src/lib/CounterDemo/Counter.tsx
+++ src/lib/CounterDemo/Counter.tsx
@@ -2,7 +2,6 @@ import type {FC} from 'react';
 import {useEffect} from 'react';
 import {throttle} from 'lodash';
 
-import classes from './Counter.module.css';
 import useLogic from './useLogic';
 
 export type Props = {
@@ -20,9 +19,12 @@ export const Counter: FC<Props> = ({initialValue = 0}) => {
         runner();
     }, []);
     return (
-        <div className={classes.counter}>
-            <h2 className={classes.header}>Counter</h2>
-            <button className={classes.button} type="button" onClick={incrementCount}>
+        <div className="w-60 border border-slate-300 p-6 text-center">
+            <h2 className="mb-3 text-2xl">Counter</h2>
+            <button
+                className="mb-6 rounded-lg bg-teal-600 px-6 py-3 text-base text-white drop-shadow-md active:relative active:left-0.5 active:top-0.5 active:drop-shadow-none"
+                type="button"
+                onClick={incrementCount}>
                 Increment by one
             </button>
             <div>
